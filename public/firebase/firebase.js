0e38d23d00f8cd967b3e7b5aa5cb9ae2caffdd5a
Dandy Vo <vodand12@gmail.com>
2020-02-29 17:29:42 -0800

    updated methods with username and pw

--- public/firebase/firebase.js
+++ public/firebase/firebase.js
@@ -1,19 +1,11 @@
 var user = firebase.auth().currentUser;
 var db = firebase.firestore();
 
-web3 = new Web3(window.ethereum);
-console.log(web3.eth.accounts[0]);
-
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
+        var address = user.address;
+        var password = user.password;
         // ...
     }
     else {
@@ -24,9 +16,9 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function signIn() {
     console.log("Attempting to sign in...");
-    var email = $("#login_email").val();
-    var password = $("#login_password").val();
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    var userName = $("#login_username").val();
+    var userPass = $("#login_password").val();
+    firebase.auth().signInWithEmailAndPassword(userName, userPass).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -36,16 +28,11 @@ function signIn() {
 }
 
 function signUpUser() {
-    var firstName = $('#first_name').val();
-    var lastName = $('#last_name').val();
-    var fullName = firstName + " " + lastName;
-    var userEmail = $('#email_field').val().toLowerCase();
+    var userName = $('#username').val();
     var userPass = $('#password_field').val();
     var userPassConf = $('#password_conf_field').val();
-    var userSSN = $('#user_ssn').val();
-
     if (userPassConf == userPass) {
-        firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(userName, userPass).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -53,20 +40,16 @@ function signUpUser() {
         // Do other stuff??
         });
         
-        db.collection("users").doc(userEmail).get()
+        db.collection("users").doc(userName).get()
         .then((docSnapshot) => {
             if (docSnapshot.exists) {
-                console.log("User exists already.");
+                console.log("User Name exists already.");
             }
             else {
-                db.collection("users").doc(userEmail).set({
-                    name: fullName,
-                    type: "user",
-                    email: userEmail,
-                    ssn: userSSN,
-                    balance: "$0.00",
-                    numReviews: 0,
-                    key: keys[keynum],
+                const { Wallet } = require("xpring-js");
+                db.collection("users").doc(userName).set({
+                    address: null,
+                    password: userPass,
                     dateCreated: firebase.firestore.FieldValue.serverTimestamp()
                 })
                 .then(function() {
@@ -83,74 +66,74 @@ function signUpUser() {
     }
 }
 
-function signUpCompany() {
-    var companyName = $('#company_name').val();
-    var companyEmail = $('#email_field').val().toLowerCase();
-    var companyPass = $('#password_field').val();
-    var companyPassConf = $('#password_conf_field').val();
-    var companyBio = $('#company_bio').val();
-    var companyGoal1 = $('#company_goal1').val();
-    var companyGoal2 = $('#company_goal2').val();
-    var companyGoal3 = $('#company_goal3').val();
+// function signUpCompany() {
+//     var companyName = $('#company_name').val();
+//     var companyEmail = $('#email_field').val().toLowerCase();
+//     var companyPass = $('#password_field').val();
+//     var companyPassConf = $('#password_conf_field').val();
+//     var companyBio = $('#company_bio').val();
+//     var companyGoal1 = $('#company_goal1').val();
+//     var companyGoal2 = $('#company_goal2').val();
+//     var companyGoal3 = $('#company_goal3').val();
 
 
-    if (companyPassConf == companyPass) {
-        firebase.auth().createUserWithEmailAndPassword(companyEmail, companyPass).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+//     if (companyPassConf == companyPass) {
+//         firebase.auth().createUserWithEmailAndPassword(companyEmail, companyPass).catch(function(error) {
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
         
-        db.collection("company").doc(companyEmail).get().then((docSnapshot) => {
-            if (docSnapshot.exists) {
-                window.alert("Error : " + errorMessage + `. This person is of type Company.`);
-            }
-            else {
-                window.alert("Error : " + errorMessage + `. This person is of type User.`);
-            }
-        })
+//         db.collection("company").doc(companyEmail).get().then((docSnapshot) => {
+//             if (docSnapshot.exists) {
+//                 window.alert("Error : " + errorMessage + `. This person is of type Company.`);
+//             }
+//             else {
+//                 window.alert("Error : " + errorMessage + `. This person is of type User.`);
+//             }
+//         })
 
-        // Do other stuff??
-        });
+//         // Do other stuff??
+//         });
 
-        db.collection("companies").doc(companyEmail).get()
-        .then((docSnapshot) => {
-            if (docSnapshot.exists) {
-                console.log("company exists already.");
-            }
-            else {
-                db.collection("users").doc(companyEmail).get()
-                .then((docSnapshot) => {
-                    if (docSnapshot.exists) {
-                        console.log("User exists already.")
-                    }
-                    else {
-                        db.collection("companies").doc(companyEmail).set({
-                            name: companyName,
-                            type: "company",
-                            email: companyEmail,
-                            bio: companyBio,
-                            moneyRaised: "$0.00",
-                            numReviews: 0,
-                            goals: [companyGoal1, companyGoal2, companyGoal3],
-                            photos: [],
-                            key: keys[keynum],
-                            dateCreated: firebase.firestore.FieldValue.serverTimestamp()
-                        })
-                        .then(function() {
-                            console.log("Document successfully written!");
-                        })
-                        .catch(function(error) {
-                            console.error("Error writing document: ", error);
-                        });
-                    }
-                })
-            }
-        });
-    }
-    else {
-        window.alert("Error : Passwords do not match");
-    }
-}
+//         db.collection("companies").doc(companyEmail).get()
+//         .then((docSnapshot) => {
+//             if (docSnapshot.exists) {
+//                 console.log("company exists already.");
+//             }
+//             else {
+//                 db.collection("users").doc(companyEmail).get()
+//                 .then((docSnapshot) => {
+//                     if (docSnapshot.exists) {
+//                         console.log("User exists already.")
+//                     }
+//                     else {
+//                         db.collection("companies").doc(companyEmail).set({
+//                             name: companyName,
+//                             type: "company",
+//                             email: companyEmail,
+//                             bio: companyBio,
+//                             moneyRaised: "$0.00",
+//                             numReviews: 0,
+//                             goals: [companyGoal1, companyGoal2, companyGoal3],
+//                             photos: [],
+//                             key: keys[keynum],
+//                             dateCreated: firebase.firestore.FieldValue.serverTimestamp()
+//                         })
+//                         .then(function() {
+//                             console.log("Document successfully written!");
+//                         })
+//                         .catch(function(error) {
+//                             console.error("Error writing document: ", error);
+//                         });
+//                     }
+//                 })
+//             }
+//         });
+//     }
+//     else {
+//         window.alert("Error : Passwords do not match");
+//     }
+// }
 
 function signOut() {
     firebase.auth().signOut();
@@ -189,41 +172,41 @@ firebase.auth().onAuthStateChanged(function(user) {
     }
 });
 
-function getNavName() {
-    var user = firebase.auth().currentUser;
+// function getNavName() {
+//     var user = firebase.auth().currentUser;
 
-    db.collection("users").doc(user.email).get()
-    .then(function(doc) {
-        if (doc.exists) {
-            let data = doc.data();
-            console.log(data["name"]);
-            name = data["name"];
-            loadNavName(name);
-        }
-        else {
-            db.collection("companies").doc(user.email).get()
-            .then(function(doc) {
-                if (doc.exists) {
-                    let data = doc.data();
-                    console.log(data["name"]);
-                    name = data["name"];
-                    loadNavName(name);
-                }
-                else {
-                    console.log("No such document!");
-                }
-            })
-        }
-    })
-    .catch(function(error) {
-        console.log("Error getting document:", error);
-    });
-}
+//     db.collection("users").doc(user.email).get()
+//     .then(function(doc) {
+//         if (doc.exists) {
+//             let data = doc.data();
+//             console.log(data["name"]);
+//             name = data["name"];
+//             loadNavName(name);
+//         }
+//         else {
+//             db.collection("companies").doc(user.email).get()
+//             .then(function(doc) {
+//                 if (doc.exists) {
+//                     let data = doc.data();
+//                     console.log(data["name"]);
+//                     name = data["name"];
+//                     loadNavName(name);
+//                 }
+//                 else {
+//                     console.log("No such document!");
+//                 }
+//             })
+//         }
+//     })
+//     .catch(function(error) {
+//         console.log("Error getting document:", error);
+//     });
+// }
 
-function loadNavName(name) {
-    var div = document.getElementById('profile-name');
-    console.log(`name is ${name}`);
+// function loadNavName(name) {
+//     var div = document.getElementById('profile-name');
+//     console.log(`name is ${name}`);
 
-    // replace text in HTML string:
-    div.innerHTML = name;
-}
+//     // replace text in HTML string:
+//     div.innerHTML = name;
+// }
